753b3624a73c02573bb16d04b68958da6e1d1d18
Tomas Barton <tomas.barton@fit.cvut.cz>
2014-11-05 19:28:02 +0100

    devise integration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141105164215) do
+ActiveRecord::Schema.define(version: 20141105180808) do
 
   create_table "authors", force: true do |t|
     t.string   "firstname"
@@ -39,5 +39,23 @@ ActiveRecord::Schema.define(version: 20141105164215) do
 
   add_index "theses", ["author_id"], name: "index_theses_on_author_id"
   add_index "theses", ["faculty_id"], name: "index_theses_on_faculty_id"
+
+  create_table "users", force: true do |t|
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
 end
